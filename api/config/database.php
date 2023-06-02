6911e9a5c4824ae5d410a0941be2e6384bd55122
Marc0ka01 <okaariel01@gmail.com>
2023-06-02 15:18:30 +0000

    project deploy

--- api/config/database.php
+++ api/config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 // Ouvrir une connexion PDO à la base de données
-$dbHost = $_ENV["DB_HOST"];
-$dbName = $_ENV["DB_NAME"];
-$dbUser = $_ENV["DB_USER"];
-$dbPassword = $_ENV["DB_PASSWORD"];
+$dbHost = "sql305.epizy.com";
+$dbName = "epiz_34341394_simplon";
+$dbUser = "epiz_34341394";
+$dbPassword = "FAyuXmUcwH";
 $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
--- registration_project/src/constant.js
+++ registration_project/src/constant.js
@@ -1,4 +1,4 @@
 // déclaration de variable qui serviront partout dans le projet
-export const API_INSERT_URL = 'http://apisimplon/insert.php';
-export const API_SHOW_URL = 'http://apisimplon/read.php';
-export const API_SEARCH_URL = "http://apisimplon/search.php";
+export const API_INSERT_URL = 'https://simplon-api.42web.io/insert.php';
+export const API_SHOW_URL = 'https://simplon-api.42web.io/read.php';
+export const API_SEARCH_URL = "https://simplon-api.42web.io/search.php";
